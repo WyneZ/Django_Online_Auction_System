107cb99ba791a1377559ff725b9372e6e6ff6ba9
WyneZ <wynekyawhan@gmail.com>
2023-10-24 00:44:53 +0630

    Add Category & Item Tables

--- OAS_1_reg_login/settings.py
+++ OAS_1_reg_login/settings.py
@@ -123,11 +123,13 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 
-# STATICFILES_DIRS = [ BASE_DIR / 'static']
+STATICFILES_DIRS = [ BASE_DIR / 'static']
 
 # To upload images
 MEDIA_URL = "/images/"
 MEDIA_ROOT = BASE_DIR / 'static/images'
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- app_reg_login/models.py
+++ app_reg_login/models.py
@@ -18,3 +18,33 @@ class User(AbstractUser):
 
     def __str__(self):
         return self.name
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
+class Item(models.Model):
+    seller = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+
+    name = models.CharField(max_length=200, null=False)
+    description = models.TextField(null=True, blank=True)
+    reverse_price = models.IntegerField(max_length=20, null=False)
+    item_condition = models.CharField(max_length=100)
+    start_date = models.DateTimeField(auto_now_add=True)
+    # end_date =
+
+    class Meta:
+        ordering = ['-start_date']
+
+
+
+
+
+
+
+
